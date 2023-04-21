32040e58cc755a0adab59741f75c2ad26f83d4c3
A.A <803674+monolithed@users.noreply.github.com>
2023-04-21 15:45:28 +0800

    Update to_numeric.hpp

--- to_numeric.hpp
+++ to_numeric.hpp
@@ -1,11 +1,8 @@
 // -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
 
 /**
- * to_numeric.hpp
- *
  * Converts a string value to numeric type
  *
- * @author: Alexander Guinness
  * @version: 0.0.1
  * @license: MIT
  * @date: Thu Mon 5 21:57:00 2012
@@ -20,8 +17,7 @@
 
 #include <type_traits>
 
-namespace nott
-{
+namespace nott {
 	/*
 	 * to_numeric
 	 *
@@ -63,80 +59,70 @@ namespace nott
 
 	/** @name std::string / const char * -> unsigned int, short int */
 	template<class T>
-		static inline T to_numeric(const std::string &value, size_t *index = 0, int base = 10)
-		{
+		static inline T to_numeric(const std::string &value, size_t *index = 0, int base = 10) {
 			return static_cast<T>(std::stoi(value, index, base));
 		}
 	;
 
 	/** @name std::string / const char * -> int */
 	template<>
-		inline int to_numeric<int>(const std::string &value, size_t *index, int base)
-		{
+		inline int to_numeric<int>(const std::string &value, size_t *index, int base) {
 			return std::stoi(value, index, base);
 		}
 	;
 
 	/** @name std::string / const char * -> long */
 	template<>
-		inline long to_numeric<long>(const std::string &value, size_t *index, int base)
-		{
+		inline long to_numeric<long>(const std::string &value, size_t *index, int base) {
 			return std::stol(value, index, base);
 		}
 	;
 
 	/** @name std::string / const char * -> long long */
 	template<>
-		inline long long to_numeric<long long>(const std::string &value, size_t *index, int base)
-		{
+		inline long long to_numeric<long long>(const std::string &value, size_t *index, int base) {
 			return std::stoll(value, index);
 		}
 	;
 
 	/** @name std::string / const char * -> unsigned long */
 	template<>
-		inline unsigned long to_numeric<unsigned long>(const std::string &value, size_t *index, int base)
-		{
+		inline unsigned long to_numeric<unsigned long>(const std::string &value, size_t *index, int base) {
 			return std::stoul(value, index, base);
 		}
 	;
 
 	/** @name std::string / const char * -> unsigned long long */
 	template<>
-		inline unsigned long long to_numeric<unsigned long long>(const std::string &value, size_t *index, int base)
-		{
+		inline unsigned long long to_numeric<unsigned long long>(const std::string &value, size_t *index, int base) {
 			return std::stoull(value, index, base);
 		}
 	;
 
 	/** @name std::string / const char * -> float */
 	template<>
-		inline float to_numeric<float>(const std::string &value, size_t *index, int base)
-		{
+		inline float to_numeric<float>(const std::string &value, size_t *index, int base) {
 			return std::stof(value, index);
 		}
 	;
 
 	/** @name std::string / const char * -> double */
 	template<>
-		inline double to_numeric<double>(const std::string &value, size_t *index, int base)
-		{
+		inline double to_numeric<double>(const std::string &value, size_t *index, int base) {
 			return std::stod(value, index);
 		}
 	;
 
 	/** @name std::string / const char * -> long double */
 	template<>
-		inline long double to_numeric<long double>(const std::string &value, size_t *index, int base)
-		{
+		inline long double to_numeric<long double>(const std::string &value, size_t *index, int base) {
 			return std::stold(value, index);
 		}
 	;
 
 	/** @name arithmetic cast */
 	template<class to_type, class from_type>
-		static inline typename std::enable_if<std::is_arithmetic<from_type>::value, from_type>::type to_numeric(const from_type &value)
-		{
+		static inline typename std::enable_if<std::is_arithmetic<from_type>::value, from_type>::type to_numeric(const from_type &value) {
 			return static_cast<to_type>(value);
 		}
 	;
